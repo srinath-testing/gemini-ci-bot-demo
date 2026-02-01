df5081601a77f9f96f76830ce3541ed25e8c5fa4
Srinath0916 <srinath0916@gmail.com>
2026-02-01 12:53:18 +0530

    Update QA test - should trigger bot with OpenWISP QA recommendations

--- openwisp_qa_test.py
+++ openwisp_qa_test.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-"""Test file to trigger OpenWISP QA workflow recommendations"""
+"""Test file to trigger OpenWISP QA workflow recommendations - UPDATED"""
 
 # Intentionally bad formatting to trigger QA failures
-def   badly_formatted_function(  ):
-    x=1+2
-    y    =    3   +   4
+def   badly_formatted_function_v2(  ):
+    x=1+2+3
+    y    =    4   +   5   +   6
     return x,y
 
 class BadlyFormattedClass:
-    def __init__(self,param1,param2):
+    def __init__(self,param1,param2,param3):
         self.param1=param1
         self.param2=param2
-    def method_with_bad_spacing(self):
-        result=self.param1+self.param2
+        self.param3=param3
+    def method_with_bad_spacing_v2(self):
+        result=self.param1+self.param2+self.param3
         return result
 
 if __name__=="__main__":
-    obj=BadlyFormattedClass(1,2)
-    print(obj.method_with_bad_spacing())
+    obj=BadlyFormattedClass(1,2,3)
+    print("Result:",obj.method_with_bad_spacing_v2())
